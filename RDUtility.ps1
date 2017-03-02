b3c63bd3b6601e9eb98b8e0c7fecc30b6d605fea
Peter M Taylor <PeterMTaylor@users.noreply.github.com>
2017-03-02 22:29:28 +1100

    Create RDUtility.ps1

---
+++ RDUtility.ps1
@@ -0,0 +1,25 @@
+function RDUtility
+{
+<#
+    .SYNOPSIS
+		Function to detect your bitness of your PC in order to diagnose and correct installation issues report in Rubberduck-VBA
+	
+	.DESCRIPTION
+		Function to check which system you have 32-bit or 64-bit to check your installation settings.
+	
+	.PARAMETER Unknown
+		Specifies the PowerShell Object
+	
+	.PARAMETER Property
+		Specifies the property to copy. Needs detail later.
+	
+	.EXAMPLE
+		PS C:\> RDUtility
+	
+	.NOTES
+		Peter M Taylor for Rubberduck-VBA project
+    https://github.com/rubberduck-vba/Rubberduck
+    @rubberduckvba
+#>
+     Return write-output 'Hello World!' 
+}
